e804a54e0df2e770f05e0cfbde4387b91f5382c2
Aneesmanzoor <Developeranees>
2019-09-25 11:26:30 +0500

    First

---
+++ build/precache-manifest.1128918130804728c2e2cfc52a88fb12.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8a9c1e5ebb880b6bc209d87df0236c8c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "89a7ba73be7f4279ca83",
+    "url": "/static/css/main.da76ae6e.chunk.css"
+  },
+  {
+    "revision": "4f7c7c589ce4499ca59c",
+    "url": "/static/js/2.defdc1cb.chunk.js"
+  },
+  {
+    "revision": "89a7ba73be7f4279ca83",
+    "url": "/static/js/main.4be4c66e.chunk.js"
+  },
+  {
+    "revision": "7bb79af6e78aea0f5b22",
+    "url": "/static/js/runtime-main.545cecad.js"
+  }
+]);
---
+++ index.js
@@ -0,0 +1,101 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const cors = require('cors')
+const path = require('path');
+
+// Initialization
+
+
+const app = express()
+
+app.use(cors())
+app.use(express.json())
+
+
+// Schema Mongoose
+
+const Schema = mongoose.Schema;
+
+const todoSchema =  Schema({
+    todo : {
+        type: String,
+        required : true
+    }
+},{timestamps : true})
+
+
+const Todo = mongoose.model('todo',todoSchema)
+
+
+// Api's
+
+// React Serve
+
+app.use(express.static(path.join(__dirname, 'build')));
+
+app.get('/', function (req, res) {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
+
+// Get Request
+
+app.get('/get_todo',(req,res)=>{
+    Todo.find().then((data)=>{
+        res.json(data)
+    })
+})
+
+// Post Req
+
+
+app.post('/add_todo',(req,res) => {
+    
+    const {todo} = req.body;
+
+    const todos = {todo}
+
+    const newTodo = new Todo(todos)
+
+    newTodo.save().then(()=>{
+        res.send('Todo Added')
+    }).catch((e)=>{
+        res.status(400).json(e)
+    })
+
+})
+
+// Delete Req
+app.delete('/delete_todo/:id',(req,res)=>{
+    Todo.findByIdAndDelete({ _id : req.params.id }).then(()=>{
+        res.send('User Deleted')
+    })
+
+})
+
+// Put Request
+
+
+app.put('/update_todo/:id',(req,res)=>{
+    const {todo} = req.body;
+
+    Todo.findByIdAndUpdate({ _id : req.params.id } , {todo}).then(()=>{
+        res.send('User Updated')
+    })
+})
+
+// Mongoose Connect
+
+mongoose.connect('mongodb+srv://anees:anees7890@aneesmanzoor-kjrae.gcp.mongodb.net/todo?retryWrites=true&w=majority',{useNewUrlParser : true , useUnifiedTopology: true ,'useFindAndModify': false}).then(()=>{
+    console.log('DB Connected')
+})
+// Listen
+
+const PORT =  process.env.PORT || 4000;
+app.listen(PORT , ()=>{
+    console.log('Server is Listening On Port : ' + PORT)
+})
+
+
+
+
